d33f31a7b92be3063ab29e426943ef60b02b3db8
mooziii <63669478+mooz-lego@users.noreply.github.com>
2021-01-30 15:25:25 +0100

    Add soundtack duration

--- src/main/java/de/polylymer/satiscraft/audio/SoundTrack.java
+++ src/main/java/de/polylymer/satiscraft/audio/SoundTrack.java
@@ -4,18 +4,22 @@ import org.bukkit.Sound;
 
 public enum SoundTrack {
 
-    MAIN_MENU(Sound.ENTITY_FOX_TELEPORT),
-    EARLY_DEVELOPMENT(Sound.AMBIENT_CRIMSON_FOREST_MOOD),
-    SATISFACTORY(Sound.AMBIENT_UNDERWATER_ENTER),
-    FLOURISH_LAND(Sound.AMBIENT_WARPED_FOREST_MOOD);
+    MAIN_MENU(Sound.ENTITY_FOX_TELEPORT, 292*20L),
+    EARLY_DEVELOPMENT(Sound.AMBIENT_UNDERWATER_ENTER,460*20L),
+    SATISFACTORY(Sound.AMBIENT_CRIMSON_FOREST_MOOD,195*20L),
+    FLOURISH_LAND(Sound.AMBIENT_WARPED_FOREST_MOOD,448*20L);
 
     private Sound sound;
+    private Long duration;
 
-    SoundTrack(Sound sound) {
+    SoundTrack(Sound sound, Long duration) {
         this.sound = sound;
+        this.duration = duration;
     }
 
     public Sound getSound() {
         return sound;
     }
+
+    public Long getDuration() {return duration;}
 }
