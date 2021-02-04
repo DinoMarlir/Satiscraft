9f4e4af047ad2f481e4d4a57cc1bb8c340d914d7
mooziii <63669478+mooz-lego@users.noreply.github.com>
2021-02-04 16:16:22 +0100

    Migrate Mod to Application

--- src/main/java/de/polylymer/satiscraft/modding/Mod.java
+++ src/main/java/de/polylymer/satiscraft/modding/Mod.java
@@ -1,25 +1,28 @@
 package de.polylymer.satiscraft.modding;
 
+import mooziii.plugin.BSpigotApplication;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public abstract class Mod extends JavaPlugin {
+public abstract class Mod extends BSpigotApplication {
 
     public abstract void finishInit();
+
     public abstract void initialize();
-    public abstract void shutdown();
+
+    public abstract void closeAndClean();
 
     @Override
-    public void onLoad() {
+    public void load() {
         initialize();
     }
 
     @Override
-    public void onDisable() {
-        shutdown();
+    public void startup() {
+        finishInit();
     }
 
     @Override
-    public void onEnable() {
-        finishInit();
+    public void shutdown() {
+        closeAndClean();
     }
 }
