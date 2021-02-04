f63993d96c987046cd703cec034f26d28cab56cf
mooziii <63669478+mooz-lego@users.noreply.github.com>
2021-02-04 12:43:18 +0100

    Create Metadata.java

---
+++ src/main/java/de/polylymer/satiscraft/identifier/metadata/Metadata.java
@@ -0,0 +1,38 @@
+package de.polylymer.satiscraft.identifier.metadata;
+
+public class Metadata {
+
+    public static final String BREAKABLE = "breakable";
+    public static final String BUILDING_BLOCK = "buildingBlock";
+    public static final String IRON_NODE_PART = "ironNodePart";
+    public static final String IRON_NODE_CORE = "ironNodeCore";
+    public static final String IRON_NODE_BLOCK = "ironNodeBlock";
+    public static final String COPPER_NODE_PART = "copperNodePart";
+    public static final String COPPER_NODE_CORE = "copperNodeCore";
+    public static final String COPPER_NODE_BLOCK = "copperNodeBlock";
+    public static final String LIMESTONE_NODE_PART = "limestoneNodePart";
+    public static final String LIMESTONE_NODE_CORE = "limestoneNodeCore";
+    public static final String LIMESTONE_NODE_BLOCK = "limestoneNodeBlock";
+    public static final String COAL_NODE_PART = "coalNodePart";
+    public static final String COAL_NODE_CORE = "coalNodeCore";
+    public static final String COAL_NODE_BLOCK = "coalNodeBlock";
+    public static final String SAM_NODE_PART = "samNodePart";
+    public static final String SAM_NODE_CORE = "samNodeCore";
+    public static final String SAM_NODE_BLOCK = "samNodeBlock";
+    public static final String URAN_NODE_PART = "uranNodePart";
+    public static final String URAN_NODE_CORE = "uranNodeCore";
+    public static final String URAN_NODE_BLOCK = "uranNodeBlock";
+    public static final String CATERIUM_NODE_PART = "cateriumNodePart";
+    public static final String CATERIUM_NODE_CORE = "cateriumNodeCore";
+    public static final String CATERIUM_NODE_BLOCK = "cateriumNodeBlock";
+    public static final String BAUXITE_NODE_PART = "bauxiteNodePart";
+    public static final String BAUXITE_NODE_CORE = "bauxiteNodeCore";
+    public static final String BAUXITE_NODE_BLOCK = "bauxiteNodeBlock";
+    public static final String SULFUR_NODE_PART = "sulfurNodePart";
+    public static final String SULFUR_NODE_CORE = "sulfurNodeCore";
+    public static final String SULFUR_NODE_BLOCK = "sulfurNodeBlock";
+    public static final String QUARTZ_NODE_PART = "quartzNodePart";
+    public static final String QUARTZ_NODE_CORE = "quartzNodeCore";
+    public static final String QUARTZ_NODE_BLOCK = "quartzNodeBlock";
+    
+}
