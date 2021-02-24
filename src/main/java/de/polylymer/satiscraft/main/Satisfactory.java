ba805a31c8cdd12fe2989945defe95047b61df69
mooziii <63669478+mooz-lego@users.noreply.github.com>
2021-02-24 22:00:16 +0100

    Update Satisfactory.java

--- src/main/java/de/polylymer/satiscraft/main/Satisfactory.java
+++ src/main/java/de/polylymer/satiscraft/main/Satisfactory.java
@@ -1,13 +1,21 @@
 package de.polylymer.satiscraft.main;
 
+import de.polylymer.satiscraft.io.ISaveGame;
+import de.polylymer.satiscraft.io.ISaveGameBuilder;
 import mooziii.annotation.PluginApp;
 import mooziii.config.Config;
 import mooziii.plugin.BSpigotApplication;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Date;
 
 @PluginApp(main = Satisfactory.class)
 public class Satisfactory extends BSpigotApplication {
+
     private static Satisfactory satisfactory;
+    private static ISaveGame saveGame;
+
     @Override
     public String name() {
         return "Satiscraft";
@@ -36,10 +44,12 @@ public class Satisfactory extends BSpigotApplication {
     @Override
     public void startup() {
         satisfactory = this;
+        saveGame = new ISaveGameBuilder().setLastModification(new Date()).build();
     }
 
     @Override
     public void shutdown() {
+        //write heap to temp.yml and read temp.yml and put stuff back on heap
         satisfactory = null;
     }
 }
