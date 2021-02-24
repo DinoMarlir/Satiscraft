c23974841646bf06643c8b14ab5943f34dbd5201
mooziii <63669478+mooz-lego@users.noreply.github.com>
2021-02-24 22:00:04 +0100

    Create ISaveGameWriter.java

---
+++ src/main/java/de/polylymer/satiscraft/io/ISaveGameWriter.java
@@ -0,0 +1,31 @@
+package de.polylymer.satiscraft.io;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ISaveGameWriter {
+
+    private final File file;
+    private final YamlConfiguration yamlConfiguration;
+
+    public ISaveGameWriter(ISaveGame saveGame) {
+        file = saveGame.getFile();
+        yamlConfiguration = YamlConfiguration.loadConfiguration(file);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <V> void write(String key, V... value) {
+        try {
+            yamlConfiguration.set(key,value);
+            yamlConfiguration.set("last-modify", new SimpleDateFormat("mm:HH, dd.MM.yyyy").format(new Date()));
+            yamlConfiguration.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
