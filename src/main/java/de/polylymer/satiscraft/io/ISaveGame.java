7d826dd09b29c5db47de753b4a20915b289377a4
mooziii <63669478+mooz-lego@users.noreply.github.com>
2021-02-24 21:59:54 +0100

    Create ISaveGame.java

---
+++ src/main/java/de/polylymer/satiscraft/io/ISaveGame.java
@@ -0,0 +1,27 @@
+package de.polylymer.satiscraft.io;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+public class ISaveGame {
+
+    private UUID owner;
+    private Date lastModified;
+    private File file;
+
+    public ISaveGame(File file) {
+        this.file = file;
+        this.lastModified = getReader().read("last-modify");
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public ISaveGameReader getReader() {
+        return new ISaveGameReader(this);
+    }
+
+}
