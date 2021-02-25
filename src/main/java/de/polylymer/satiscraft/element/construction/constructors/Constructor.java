82e47896d98a55d92181c41b646ae6aaa8e50628
mooziii <63669478+mooz-lego@users.noreply.github.com>
2021-02-25 14:47:59 +0100

    Create Constructor.java

---
+++ src/main/java/de/polylymer/satiscraft/element/construction/constructors/Constructor.java
@@ -0,0 +1,15 @@
+package de.polylymer.satiscraft.element.construction.constructors;
+
+import de.polylymer.satiscraft.element.PlaceableElement;
+import de.polylymer.satiscraft.element.construction.InputOutput;
+import de.polylymer.satiscraft.element.ModularBuilding;
+
+@ModularBuilding(
+        elementOne = 0,
+        elementTwo = 1
+)
+public class Constructor implements PlaceableElement<InputOutput> {
+
+
+
+}
