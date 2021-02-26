e8b39e2c329c62578e46270718cd48421d40a94a
mooziii <63669478+mooz-lego@users.noreply.github.com>
2021-02-26 14:43:31 +0100

    Update ModularElementPlacer.java

--- src/main/java/de/polylymer/satiscraft/element/ModularElementPlacer.java
+++ src/main/java/de/polylymer/satiscraft/element/ModularElementPlacer.java
@@ -1,13 +1,17 @@
 package de.polylymer.satiscraft.element;
 
+import de.polylymer.satiscraft.main.Satisfactory;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 public interface ModularElementPlacer {
 
-    default <E> void place(Player player, E e) {
+    default <E> void place(Player player, E e, Location location) {
         ModularBuilding modularBuilding = e.getClass().getAnnotation(ModularBuilding.class);
         if(modularBuilding != null) {
-
+            String key = "building." + location.getBlockX() + "_" + location.getBlockY() + "_" + location.getBlockZ();
+            Satisfactory.getFactory().getSaveGame().getWriter().write(key + e.getClass().getName(), true);
+            Satisfactory.getFactory().getSaveGame().getWriter().write(key + e.getClass().getName() + ".recipe", null);
         } else {
 
         }
