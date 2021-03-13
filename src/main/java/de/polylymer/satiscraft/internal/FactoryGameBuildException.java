82c1f1c7e1eb30a3b2732471b240b0cadee068fd
mooziii <mooz@moozunity.de>
2021-03-13 14:32:28 +0100

    Create FactoryGameBuildException.java

---
+++ src/main/java/de/polylymer/satiscraft/internal/FactoryGameBuildException.java
@@ -0,0 +1,9 @@
+package de.polylymer.satiscraft.internal;
+
+public class FactoryGameBuildException extends Exception {
+
+    public FactoryGameBuildException(String message) {
+        super(message);
+    }
+
+}
