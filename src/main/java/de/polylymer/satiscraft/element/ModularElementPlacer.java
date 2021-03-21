e9d846b21a68bfe9dd977d725bf97e6b234b23c0
mooziii <mooz@moozunity.de>
2021-03-21 11:21:31 +0100

    Trigger Events

--- src/main/java/de/polylymer/satiscraft/element/ModularElementPlacer.java
+++ src/main/java/de/polylymer/satiscraft/element/ModularElementPlacer.java
@@ -1,6 +1,7 @@
 package de.polylymer.satiscraft.element;
 
-import de.polylymer.satiscraft.internal.FactoryGameBuildException;
+import de.polylymer.satiscraft.internal.eventmanager.EventDisplay;
+import de.polylymer.satiscraft.internal.exception.FactoryGameBuildException;
 import de.polylymer.satiscraft.internal.FactoryGameCrashReport;
 import de.polylymer.satiscraft.main.Satisfactory;
 import org.bukkit.Location;
@@ -9,14 +10,19 @@ import org.bukkit.entity.Player;
 public interface ModularElementPlacer {
 
     default <E> void place(Player player, E e, Location location) {
+        EventDisplay.addToStash("|");
         ModularBuilding modularBuilding = e.getClass().getAnnotation(ModularBuilding.class);
         if(modularBuilding != null) {
             String key = "building." + location.getBlockX() + "_" + location.getBlockY() + "_" + location.getBlockZ();
             Satisfactory.getFactory().getSaveGame().getWriter().write(key + e.getClass().getName(), true);
+            EventDisplay.addToStash("+");
             Satisfactory.getFactory().getSaveGame().getWriter().write(key + e.getClass().getName() + ".recipe", null);
+            EventDisplay.addToStash("+");
         } else {
+            EventDisplay.addToStash("-");
             new FactoryGameCrashReport(new FactoryGameBuildException("Cannot place illegally building: Building is not annotated with @ModularBuilding"), location, getClass());
         }
+        EventDisplay.addToStash("|");
     }
 
 }
