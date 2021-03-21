bc2699ef50e2b3b7c335c89c485072acc1715071
mooziii <mooz@moozunity.de>
2021-03-21 11:20:44 +0100

    Change Constructor costs from binary int to enum type

--- src/main/java/de/polylymer/satiscraft/element/construction/constructors/Constructor.java
+++ src/main/java/de/polylymer/satiscraft/element/construction/constructors/Constructor.java
@@ -1,12 +1,12 @@
 package de.polylymer.satiscraft.element.construction.constructors;
 
+import de.polylymer.satiscraft.element.Element;
 import de.polylymer.satiscraft.element.PlaceableElement;
 import de.polylymer.satiscraft.element.construction.InputOutput;
 import de.polylymer.satiscraft.element.ModularBuilding;
 
 @ModularBuilding(
-        elementOne = 0,
-        elementTwo = 1
+        elementOne = Element.AIR
 )
 public class Constructor implements PlaceableElement<InputOutput> {
 
