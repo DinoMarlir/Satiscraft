63ff53a90061117e569d84d1c23b9aac03f752fe
mooziii <mooz@moozunity.de>
2021-03-21 11:18:25 +0100

    Some reflection shit

--- src/main/java/de/polylymer/satiscraft/modding/ModLoader.java
+++ src/main/java/de/polylymer/satiscraft/modding/ModLoader.java
@@ -1,15 +1,23 @@
 package de.polylymer.satiscraft.modding;
 
+import de.polylymer.satiscraft.event.Event;
+import de.polylymer.satiscraft.internal.FactoryGameCrashReport;
+import de.polylymer.satiscraft.modding.event.Action;
+
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Set;
 
 public final class ModLoader {
 
-    private final Collection<Method> modifyMethods = new ArrayList<Method>();
+    private final static Collection<Method> modifyingMethods = new ArrayList<>();
+    private final static Collection<Method> subscribedMethods = new ArrayList<>();
+    private final static ModLoader instance = new ModLoader();
 
+    public static ModLoader getInstance() {
+        return instance;
+    }
 
     public void loadMods() throws IOException {
 
@@ -19,7 +27,25 @@ public final class ModLoader {
         for (Method method : clazz.getMethods()) {
             Modify modify = method.getAnnotation(Modify.class);
             if(modify != null) {
-                modifyMethods.add(method);
+                modifyingMethods.add(method);
+            } else {
+                Subscribe subscribe = method.getAnnotation(Subscribe.class);
+                if(subscribe != null) {
+                    subscribedMethods.add(method);
+                }
+            }
+        }
+    }
+
+    public void triggerEvent(Event event, Action action) {
+        for (Method method : subscribedMethods) {
+            Subscribe subscribe = method.getAnnotation(Subscribe.class);
+            if(subscribe.value() == event) {
+                try {
+                    method.invoke(null, action);
+                } catch (Exception e) {
+                    new FactoryGameCrashReport(e, subscribe, method.getDeclaringClass());
+                }
             }
         }
     }
