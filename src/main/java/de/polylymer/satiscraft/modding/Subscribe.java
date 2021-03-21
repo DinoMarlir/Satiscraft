794e05036cbb99c69fb4463a8faa5c7f12ad9f7f
mooziii <mooz@moozunity.de>
2021-03-21 11:22:09 +0100

    create @Subscribe modding annotation

--- src/main/java/de/polylymer/satiscraft/modding/Subscribe.java
+++ src/main/java/de/polylymer/satiscraft/modding/Subscribe.java
@@ -1,4 +1,19 @@
 package de.polylymer.satiscraft.modding;
 
+import de.polylymer.satiscraft.event.Event;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Subscribe {
+
+
+    //ERST EVENT INITTEN, DANN TRIGGERN, DANN AUSFÜHREN
+
+    Event value();
+
 }
